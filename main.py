2ebc87496111cb867f8bbb76a42ab53d3717d1be
junsu <yeo302727@gmail.com>
2026-02-21 13:02:49 +0900

    add scheduler,net2

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from datasets import load_dataset, Features, Value
 from torch.utils.data import DataLoader
 import argparse
-
-
+from timm.utils import ModelEmaV2
+from transformers import get_cosine_schedule_with_warmup
 features = Features(
     {
         "source": Value("string"),
@@ -47,6 +47,7 @@ class TinyNetworkBlock(nn.Module):
     def __init__(self, embed_dim=512, num_heads=8,att='self_att'):
         super().__init__()
         self.norm1 = nn.LayerNorm(embed_dim)
+        self.att= att
         if(att == 'self_att'):
             self.attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         elif(att == 'mlp'):
@@ -66,7 +67,11 @@ class TinyNetworkBlock(nn.Module):
 
     def forward(self, query, context):
         # maybe add pre-norm later?
-        attn_output, _ = self.attn(query, context, context)
+        if(self.att == "self_att"):
+            attn_output, _ = self.attn(query, context, context)
+        else:
+            mixed = torch.cat([query,context],dim=-1)
+            attn_output = self.attn(mixed)
         query = self.norm1(query + attn_output)
         ff_output = self.mlp(query)
         query = self.norm2(query + ff_output)
@@ -77,6 +82,7 @@ class TRM(nn.Module):
     def __init__(self, embed_dim=512, n_reasoning_steps=6, n_recursion_steps=3,att='self_att'):
         super().__init__()
         self.net = TinyNetworkBlock(embed_dim,att=att)
+        self.net2 = TinyNetworkBlock(embed_dim,att=att)
         self.n = n_reasoning_steps
         self.T = n_recursion_steps
         self.token_embedding = nn.Embedding(10, embed_dim)
@@ -94,7 +100,7 @@ class TRM(nn.Module):
         for _ in range(self.n):  # latent reasoning
             combined_context = torch.cat([x, y], dim=1)
             z = self.net(query=z, context=combined_context)
-        y = self.net(query=y, context=z)  # answer refinement
+        y = self.net2(query=y, context=z)  # answer refinement
         return y, z
 
     def forward(self, x, y, z):
@@ -112,16 +118,30 @@ def main():
     parser.add_argument('--ema',action='store_true',help='using ema')
     args = parser.parse_args()
     embed_dim = 512
-    batch_size = 16
+    batch_size = 8
     max_supervision_steps = 16  # N_sup from the paper
     max_val_batches = 1
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     trm_model = TRM(embed_dim, n_reasoning_steps=6,att = args.att).to(device)
+    ema_model = ModelEmaV2(trm_model,decay=0.999)
+    ema_model.module.eval()
     print_model_parameter_count(trm_model)
     trm_model.train()
-    optimizer = torch.optim.Adam(trm_model.parameters(), lr=1e-4)
+    optimizer = torch.optim.AdamW(trm_model.parameters(),
+    lr=1e-4,
+    betas=(0.9,0.95),
+    weight_decay=0.1)
+    warmup_step=200
+    training_step=1000
+    lr_scheduler = get_cosine_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=warmup_step,
+        num_training_steps=training_step
+
+    )
+    
     loss_fn = nn.CrossEntropyLoss()
 
     full_train = load_dataset("sapientinc/sudoku-extreme", features=features)["train"]
@@ -161,18 +181,32 @@ def main():
             else:
                 y = x.clone()
                 z = x.clone()
-            (a, b), y_hat = trm_model(x, y, z)
+                (a, b), y_hat = trm_model(x, y, z)
             loss = loss_fn(y_hat.reshape(-1, 10), answer_tokens.reshape(-1))
             loss.backward()
             optimizer.step()
-        if batch_idx % 1 == 0:
-            print(f"batch={batch_idx} loss={loss.item():.4f}")
-        if batch_idx >= 100:  # Train on a subset for demo purposes
+            lr_scheduler.step()
+            if(args.ema):
+                ema_model.update(trm_model)
+        if batch_idx % 5 == 0:
+            if(args.ema):
+                with torch.no_grad():
+                    x=ema_model.module.embed_input(question_tokens)
+                    y = x.clone()
+                    z = x.clone()
+                    (a, b), y_hat = ema_model.module(x, y, z)
+                    ema_loss = loss_fn(y_hat.reshape(-1, 10), answer_tokens.reshape(-1))
+                print(f"batch={batch_idx} loss={loss.item():.4f} ema loss={ema_loss.item():.4f}")
+            else:
+                print(f"batch={batch_idx} loss={loss.item():.4f} ")
+        if batch_idx >= training_step:  # Train on a subset for demo purposes
             break
 
     trm_model.eval()
     val_loss = 0.0
     val_correct = 0
+    ema_val_loss=0.0
+    ema_val_correct=0
     val_total = 0
     val_batches = 0
     with torch.no_grad():
@@ -191,19 +225,31 @@ def main():
             y = x.clone()
             z = x.clone()
             (_, _), y_hat = trm_model(x, y, z)
-
             loss = loss_fn(y_hat.reshape(-1, 10), answer_tokens.reshape(-1))
             val_loss += loss.item()
             preds = y_hat.argmax(dim=-1)
             val_correct += (preds == answer_tokens).sum().item()
             val_total += answer_tokens.numel()
             val_batches += 1
+            if(args.ema):
+                x = ema_model.module.embed_input(question_tokens)
+                y = x.clone()
+                z = x.clone()
+                (_, _), y_hat = ema_model.module(x, y, z)
+                loss = loss_fn(y_hat.reshape(-1, 10), answer_tokens.reshape(-1))
+                ema_val_loss += loss.item()
+                preds = y_hat.argmax(dim=-1)
+                ema_val_correct += (preds == answer_tokens).sum().item()               
             if val_batches >= max_val_batches:
                 break
 
     avg_val_loss = val_loss / max(1, val_batches)
     val_acc = val_correct / max(1, val_total)
     print(f"val_loss={avg_val_loss:.4f} val_acc={val_acc:.4f}")
+    if(args.ema):
+        ema_avg_val_loss = ema_val_loss / max(1, val_batches)
+        ema_val_acc = ema_val_correct / max(1, val_total)
+        print(f"ema_val_loss={ema_avg_val_loss:.4f} ema_val_acc={ema_val_acc:.4f}")
 
 
 if __name__ == "__main__":
